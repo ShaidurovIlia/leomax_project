a9cfafe6777c651e055f4326537239aa92d7d612
ShaidurovIlia <shaidurov.ilha@yandex.ru>
2023-09-10 20:07:46 +0200

    add components LocationForm

---
+++ src/test/java/org/example/components/LocationForm.java
@@ -0,0 +1,11 @@
+package org.example.components;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class LocationForm {
+    public static void city() {
+        $("div.modal-city-selection").shouldBe(visible);
+        $(".modal-city-selection__button_confirm").click();
+    }
+}
