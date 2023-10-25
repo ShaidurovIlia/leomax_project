29c4c26e9188e17255aba4874e7795981f30dc6b
ShaidurovIlia <shaidurov.ilha@yandex.ru>
2023-10-25 11:34:34 +0300

    add emailTest

---
+++ src/test/java/selenide/pages/EmailPage.java
@@ -0,0 +1,41 @@
+package selenide.pages;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class EmailPage extends BasePage {
+
+    private static final Faker faker = new Faker();
+    @FindBy(css = "#email")
+    private WebElement emailInput;
+    @FindBy(xpath = "//div[@class='modal-body' and text()='Вы уже подписаны на рассылку.']")
+    private WebElement subscribeMessage;
+    @FindBy(xpath = "//div[@class='modal-body' and text()='Вы успешно подписались на рассылку."
+            + " Скоро на ваш адрес электронной почты придет письмо с подтверждением.']")
+    private WebElement successMessage;
+
+    public EmailPage() {
+        driver.get("https://www.leomax.ru/");
+        PageFactory.initElements(driver, this);
+    }
+
+    public boolean subscribeWithFakerEmail() {
+        String generatedEmail = faker.internet().emailAddress();
+        emailInput.click();
+        emailInput.sendKeys(generatedEmail);
+        emailInput.sendKeys(Keys.ENTER);
+        return successMessage.isDisplayed();
+    }
+
+    public boolean subscribeWithEmail(String emailAddress) {
+        emailInput.click();
+        emailInput.sendKeys(emailAddress);
+        emailInput.sendKeys(Keys.ENTER);
+        return subscribeMessage.isDisplayed();
+    }
+
+
+}
---
+++ src/test/java/selenide/tests/EmailTest.java
@@ -0,0 +1,31 @@
+package selenide.tests;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import selenide.core.BaseTest;
+import selenide.pages.EmailPage;
+
+@Owner(value = "Илья Шайдуров")
+@DisplayName("Класс EmailTest")
+public class EmailTest extends BaseTest {
+
+    EmailPage email = new EmailPage();
+    String emailAddress = "test@mail.ru";
+
+    @DisplayName("Тест подписки с email-генератором")
+    @Description("Проверяет подписку на email рассылку с использованием генерированного email")
+    @Test
+    public void checkWithFakerEmail() {
+        Assertions.assertTrue(email.subscribeWithFakerEmail());
+    }
+
+    @DisplayName("Тест подписки с заданным email")
+    @Description("Проверяет что указынный email уже подписан на рассылку")
+    @Test
+    public void checkWithEmail() {
+        Assertions.assertTrue(email.subscribeWithEmail(emailAddress));
+    }
+}
