0d83546f10103a72a760ace82234aac6411d88f6
ShaidurovIlia <shaidurov.ilha@yandex.ru>
2024-05-22 15:33:55 +0300

    delete ChromeDriver

--- src/test/java/selenium/core/WebDriverFactory.java
+++ src/test/java/selenium/core/WebDriverFactory.java
@@ -4,20 +4,24 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 public class WebDriverFactory {
     private static final int MyTimeout = 60;
 
     public static WebDriver createChromeDriver() {
-        System.setProperty("webdriver.chrome.driver",
-                "src/test/resources/chromedriver.exe");
+        // Установка опций для ChromeDriver
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-popup-blocking");
+
+        // Создание экземпляра WebDriver с использованием настроек
         WebDriver driver = new ChromeDriver(options);
+
+        // Настройка окна и таймаутов
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(MyTimeout, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(MyTimeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(MyTimeout));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(MyTimeout));
+
         return driver;
     }
 }
